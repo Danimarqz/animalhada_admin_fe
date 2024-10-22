69b126eeabd5b9c40e9233da6dca291fe12d2dfa
Danimarqz <daniel.marquez@ciclosmontecastelo.com>
2024-10-22 19:07:14 +0200

    add login function

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,5 @@
 import type { Tables, TablesInsert, TablesUpdate } from "../types/database.types";
+import Security from "../utils/security";
 
 
 export type Product = Tables<'product'>
@@ -11,16 +12,18 @@ type APIresponse<T> = {
 }
 
 const base_url = import.meta.env.BASE_PATH
+const headers = {
+    'Content-Type': 'application/json',
+    'Origin': import.meta.env.SITE_URL,
+    'X-API-KEY': import.meta.env.API_KEY
+}
 
 export async function GetProducts(page = 1, order = false): Promise<APIresponse<Product[]>> {
     let url = `${base_url}/product`
     
     const response = await fetch(pagination(url, page, order), {
         method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Origin': import.meta.env.SITE_URL
-        }
+        headers: headers
     })
     const products: Product[] = await response.json()
     if (!response.ok) { throw new Error (response.statusText)}
@@ -33,10 +36,7 @@ export async function GetProducts(page = 1, order = false): Promise<APIresponse<
 export async function GetOne(id: number): Promise<Product> {
     const response = await fetch(`${base_url}/product/id/${id}`, {
         method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Origin': import.meta.env.SITE_URL
-        }
+        headers: headers
     })
     if (!response.ok) { throw new Error (response.statusText)}
     return await response.json() as Product
@@ -49,16 +49,47 @@ export async function GetByCategory(categories: string[], page = 1, order = fals
     url = pagination(url, page, order)
     const response = await fetch(url, {
         method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Origin': import.meta.env.SITE_URL
-        }
+        headers: headers
     })
     if (!response.ok) { throw new Error (response.statusText)}
     const products: Product[] = await response.json()
     return {
         data: products,
         total: products.length
+    }
+}
+
+export async function Login(username: string, password: string) {
+    try {
+        const security = new Security();
+        const data: string = JSON.stringify({ username, password });
+        const dataEncrypted: string | undefined = security.encrypt(data);
+
+        if (!dataEncrypted) {
+            throw new Error('Encryption failed');
+        }
+
+        const url = `${base_url}/auth`;
+
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: headers,
+            body: JSON.stringify({ data: dataEncrypted }),
+        });
+
+        const result = await response.json();
+        if (response.ok) {
+            if (result.message === 'Authenticated') {
+                console.log('Login successful:', result.message);
+                return { success: true, message: result.message };
+            }
+        } else {
+            console.error('Error during login:', result);
+            return { success: false, error: result.error || 'Login failed' };
+        }
+    } catch (error) {
+        console.error('Error during login:', error);
+        return { success: false, error: error };
     }
 }
 
--- src/env.d.ts
+++ src/env.d.ts
@@ -3,6 +3,8 @@
 interface ImportMetaEnv {
     readonly BASE_PATH: string;
     readonly SITE_URL: string;
+    readonly API_KEY: string;
+    readonly CRYPTO_KEY: string;
 }
 interface ImportMeta {
     readonly env: ImportMetaEnv;
